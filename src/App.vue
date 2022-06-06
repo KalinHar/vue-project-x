2fa7489a36a4f6e9bbf3b2f97d7c23e580b12653
KalinHar <kalix@abv.bg>
2022-06-06 16:02:17 +0300

    add token and authorization state to store

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,20 @@
 
 <script>
 import Navbar from '@/components/layout/Navbar'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
     Navbar
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+
+    if (this.$store.state.token) {
+      axios.defaults.headers.common['Authorization'] = "Token " + this.$store.state.token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
   },
 }
 </script>
