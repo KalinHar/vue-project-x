95a3268d12f4819b4837d03c1c2aa7ad972023c3
KalinHar <kalix@abv.bg>
2022-06-06 17:58:35 +0300

    add submit form to register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,26 +4,30 @@
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Register</h1>
 
-                <form>
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label>Email</label>
                         <div class="control">
-                            <input type="email" name="email" class="input">
+                            <input type="email" name="email" class="input" v-model="username">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Password</label>
                         <div class="control">
-                            <input type="password" name="password1" class="input">
+                            <input type="password" name="password1" class="input" v-model="password1">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Repeat Password</label>
                         <div class="control">
-                            <input type="password" name="password2" class="input">
+                            <input type="password" name="password2" class="input" v-model="password2">
                         </div>
+                    </div>
+
+                    <div class="notification is-danger" v-if="errors.length">
+                        <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -38,8 +42,59 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {toast} from 'bulma-toast'
 
 export default{
     name: 'Register',
+    data() {
+        return {
+            username: '',
+            password1: '',
+            password2: '',
+            errors: []
+        }
+    },
+    methods: {
+        submitForm() {
+            this.errors = []
+            if (this.username === '') {
+                this.errors.push('Missing username!')
+            }
+            if (this.password1 === '') {
+                this.errors.push('Missing password!')
+            }
+            if (this.password1 !== this.password2) {
+                this.errors.push('Passwords do not match!')
+            }
+            if (!this.errors.length) {
+                const formData = {
+                    username: this.username,
+                    password: this.password1,
+                }
+                axios.post('/api/users/', formData)
+                     .then(response => {
+                         toast({
+                             message: 'Account was created. Please Login.',
+                             type: 'is-success',
+                             dismissible: true,
+                             pauseOnHover: true,
+                             duration: 2000,
+                             position: 'bottom-right'
+                         })
+                         this.$router.push('/login')
+                     })
+                     .catch(error => {
+                         if (error.response) {
+                             for (const property in error.response.data) {
+                                 this.errors.push(`${property}:  ${error.response.data[property]}`)
+                             }
+                         } else if (error.message) {
+                             this.errors.push(`Somthing went wrong. ${error.message}`)
+                         }
+                     })
+            }
+        }
+    }
 }
 </script>
