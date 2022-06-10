dc1c5fa9b3c9e983d90248f9475c1b38395f0ca3
KalinHar <kalix@abv.bg>
2022-06-10 15:10:34 +0300

    ref pages lead , leads

--- src/views/dashboard/Lead.vue
+++ src/views/dashboard/Lead.vue
@@ -12,7 +12,7 @@
                     <h2 class="subtitle">Details</h2>
 
                     <template v-if="lead.assigned_to">
-                        <p><strong>Assigned to: </strong>{{ lead.assigned_to.username }}</p>
+                        <p><strong>Assigned to: </strong>{{ lead.assigned_to.first_name }} {{ lead.assigned_to.last_name }}</p>
                     </template>
                     <p><strong>Status: </strong>{{ lead.status }}</p>
                     <p><strong>Priority: </strong>{{ lead.priority }}</p>
